09a8808cc58b7cbfe084cada475455966b487f8a
lavreshka <lavreshka23@gmail.com>
2020-11-03 18:14:18 +0300

    Made my blog with django

---
+++ blog/blog_engine/django_blog/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.shortcuts import reverse
+from django.utils.text import slugify
+
+from transliterate import translit
+from transliterate.exceptions import LanguageDetectionError
+from time import time
+
+def gen_slug(title):
+    try:
+        new_title = translit(title, reversed = True)
+    except LanguageDetectionError:
+        new_title = title
+    new_slug = slugify(new_title, allow_unicode = True)
+    return new_slug + '-' + str(int(time()))
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    slug = models.SlugField(max_length=150, blank = True, unique=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs = {'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug':self.slug})
+
+    def get_delete_url(self):
+        return reverse('post_delete_url', kwargs={'slug':self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-date_pub']
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs = {'slug':self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug':self.slug})
+
+    def get_delete_url(self):
+        return reverse('tag_delete_url', kwargs = {'slug':self.slug})
+
+    class Meta:
+        ordering = ['title']
---
+++ blog/blog_engine/django_blog/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
+
+from .models import Post, Tag
+from .utils import *
+from .forms import TagForm, PostForm
+
+
+def posts_list(request):
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    posts_on_page = 3
+    paginator = Paginator(posts, posts_on_page)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+    'page_object':page,
+    'is_paginated':is_paginated,
+    'prev_url':prev_url,
+    'next_url':next_url,
+    }
+    return render(request, 'django_blog/index.html', context = context)
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'django_blog/tags_list.html', context = {'tags':tags})
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'django_blog/post_detail.html'
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'django_blog/tag_detail.html'
+
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'django_blog/post_create_form.html'
+    raise_exception = True
+
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'django_blog/tag_create.html'
+    raise_exception = True
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'django_blog/post_update_form.html'
+    raise_exception = True
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'django_blog/tag_update_form.html'
+    raise_exception = True
+
+class TagDelete(LoginRequiredMixin,ObjectDeleteMixin, View):
+    model = Tag
+    template = 'django_blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    raise_exception = True
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'django_blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
